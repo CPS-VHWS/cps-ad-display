ad862d37b5349bb5907551b396f3c31cd3d1f48e
minhthanhbrvt95-web <minhthanhbrvt95@gmail.com>
2026-08-27 15:18:48 +0700

    chore: update playlists via admin dashboard

--- config.js
+++ config.js
@@ -8,15 +8,12 @@ const PLAYLIST_LANDSCAPE = [
   { id: 'aCJFpFS6OPE', duration: 31, label: 'LÃÂªn ÃÂÃ¡Â»Âi smartphone mÃ¡Â»Âi', campaign: 'trade' },
   { id: 'Javr2nYWOxI', duration: 37, label: 'Mua Laptop tÃ¡ÂºÂ·ng Bali', campaign: 'trade' },
   { id: 'uu_52j5NxG8', duration: 30, label: 'BÃ¡Â»Â phÃ¡Â»Â¥ kiÃ¡Â»Ân GenZ & QuÃÂ  Limited 2026', campaign: 'trade' },
-  { id: '7doGCi1sBXY', label: 'DEMO MDM LAPTOP CAMP 2/9', campaign: 'quockhanh' }
+  { id: '7doGCi1sBXY', label: 'DEMO MDM LAPTOP CAMP 2/9', campaign: 'trade' }
 ];
 
 const PLAYLIST_PORTRAIT = [
   { id: 'YpV8qvzXvcU', duration: 41, label: 'SCamp 2026', campaign: 'scamp' },
-  { id: '-9d37qCzhAs', duration: 31, label: 'LÃÂªn ÃÂÃ¡Â»Âi Smartphone mÃ¡Â»Âi', campaign: 'trade' },
-  { id: 'Djl36n4AdLw', duration: 37, label: 'Mua laptop trÃÂºng Balo', campaign: 'trade' },
-  { id: 'MFLGVWh3R4k', duration: 30, label: 'BÃ¡Â»Â phÃ¡Â»Â¥ kiÃ¡Â»Ân GenZ vÃÂ  QuÃÂ  Limited', campaign: 'trade' },
-  { id: 'WkF7cG9sYAo', label: 'DEMO MDM ĐIỆN THOẠI CAMP 2/9', campaign: 'quockhanh' }
+  { id: 'WkF7cG9sYAo', label: 'DEMO MDM ĐIỆN THOẠI CAMP 2/9', campaign: 'trade' }
 ];
 
 const APP_CONFIG = {
